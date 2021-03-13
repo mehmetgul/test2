3f9c3935f42a7ce39ab48ad2653cbc885a34b714
Mehmet Gul <mehmetgul@gmail.com>
2021-03-13 14:54:42 -0500

    First Commit

---
+++ src/test/java/test/test1.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class test1 {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+	public void test(){
+		System.out.println("Method");
+	}
+}
